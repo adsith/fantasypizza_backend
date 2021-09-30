3daed04bf18c7c8db9d1bcea6c85513ed2ea601f
Carlos Alvarez <carlosalvarez1512@gmail.com>
2021-09-30 16:25:05 +0200

    feat: add fixture data

--- fantasypizza_backend/pizza/urls.py
+++ fantasypizza_backend/pizza/urls.py
@@ -3,5 +3,7 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('ingridient/', views.get_ingridients, name='ingridients'),
+    path('ingridient/<int:ingredient_id>', views.get_ingridient, name='ingridient')
 ]
--- fantasypizza_backend/pizza/views.py
+++ fantasypizza_backend/pizza/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world")
+
+def get_ingridients(request):
+    return HttpResponse()
+
+def get_ingridient(request, ingredient_id):
+    return HttpResponse()
